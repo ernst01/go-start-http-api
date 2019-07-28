28d43a0ead9e64e8d73d50800802cd2edfe8764a
thiebaud ernstberger <thiebaud.ernstberger@gmail.com>
2019-07-28 16:03:45 -0700

    Added OPTIONS route + Accept json header

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -6,13 +6,14 @@ import (
 
 // Routes defines all our routes
 func (s *Server) Routes() {
-	s.Router.Path("/myapi").Methods("GET").
+	s.Router.Path("/myapi").Methods("GET", "OPTIONS").
 		HandlerFunc(corsHandler(s.handleReadMyAPI()))
 }
 
 func corsHandler(h http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Accept", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		if r.Method == http.MethodOptions {
 			w.Header().Set("Access-Control-Allow-Headers", "Authorization")
